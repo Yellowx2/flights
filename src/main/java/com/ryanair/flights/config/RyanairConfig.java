878b722875a42e398815ccd65d407d236e3ad146
Víctor Huang Huang <vic-16@hotmail.com>
2021-06-12 18:45:00 +0200

    Added bean annotation

--- src/main/java/com/ryanair/flights/config/RyanairConfig.java
+++ src/main/java/com/ryanair/flights/config/RyanairConfig.java
@@ -3,11 +3,13 @@ package com.ryanair.flights.config;
 import com.ryanair.flights.model.UrlDto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-import lombok.Getter;
+import lombok.Data;
 
+@Configuration
 @ConfigurationProperties(prefix = "ryanair")
-@Getter
+@Data
 public class RyanairConfig {
 
     private UrlDto route;
