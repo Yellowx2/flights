42f1cfe9e944662ffcc7867e7ff57a3f098e53e4
Víctor Huang Huang <vic-16@hotmail.com>
2021-06-17 00:08:13 +0200

    Added dependencies for Jackson parsing

--- src/main/java/com/ryanair/flights/config/RyanairConfig.java
+++ src/main/java/com/ryanair/flights/config/RyanairConfig.java
@@ -1,8 +1,14 @@
 package com.ryanair.flights.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.ryanair.flights.model.UrlDto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
@@ -18,4 +24,20 @@ public class RyanairConfig {
     private UrlDto route;
 
     private UrlDto schedule;
+
+    /**
+     * This bean is supposed to allow Jackson to process Java 8 DateTimes, but I
+     * don't know why it doesn't work as intended. I tried it in several ways, using
+     * Module bean, Spring properties but I couldn't obtain a satisfactory result
+     * from it. I'll leave it for the future
+     *
+     * @return ObjectMapper bean
+     */
+    @Bean
+    public ObjectMapper mapper() {
+        return JsonMapper.builder()
+            .addModules(new ParameterNamesModule(), new Jdk8Module(), new JavaTimeModule())
+            // .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .build();
+    }
 }
